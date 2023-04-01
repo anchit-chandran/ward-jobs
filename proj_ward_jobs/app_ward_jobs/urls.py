8837ebd1fe69326f92204f89eaaa25d38e160305
Anchit <anchit97123@gmail.com>
2023-04-01 18:03:35 +0100

    adds nav bar and job styling

--- proj_ward_jobs/app_ward_jobs/urls.py
+++ proj_ward_jobs/app_ward_jobs/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('view-all-jobs', views.all_jobs, name='all-jobs'),
 ]
--- proj_ward_jobs/app_ward_jobs/views.py
+++ proj_ward_jobs/app_ward_jobs/views.py
@@ -3,12 +3,24 @@ from django.shortcuts import render
 from .models import Patient, Doctor, Job
 
 # Create your views here.
+
+
 def home(request):
-    
+
     jobs = Job.objects.all()
-    
+
     context = {
-        'jobs' : jobs
+        'jobs': jobs
     }
-    
     return render(request, template_name='app_ward_jobs/home.html', context=context)
+
+
+def all_jobs(request):
+
+    jobs = Job.objects.all()
+
+    context = {
+        'jobs': jobs
+    }
+
+    return render(request, template_name='app_ward_jobs/all_jobs_list.html', context=context)
